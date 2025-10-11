674a80ab9fe5ac0fe683a6e7344cb4b3efe698b5
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2025-10-10 20:03:26 -0500

    chore: add unpdf library and implement basic PDF text extraction

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,18 +1,19 @@
 import { redirect } from "next/navigation";
-
+import  { unPDFTest}  from "@/lib/pdftest";
 import { createClient } from "@/lib/supabase/server";
 
 export default async function ChatPage() {
-  const supabase = await createClient();
+	const supabase = await createClient();
+  const text = await unPDFTest("/home/wavefire/development/utsbi/legal-rag/Legal/All Site Files/enTouch Preferred Wiring Standard.pdf");
 
-  const { data, error } = await supabase.auth.getClaims();
-  if (error || !data?.claims) {
-    redirect("/");
-  }
+	const { data, error } = await supabase.auth.getClaims();
+	if (error || !data?.claims) {
+		redirect("/");
+	}
 
-  return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div>Placeholder</div>
-    </div>
-  );
+	return (
+		<div className="flex-1 w-full flex flex-col gap-12">
+			<div>{text}</div>
+		</div>
+	);
 }
